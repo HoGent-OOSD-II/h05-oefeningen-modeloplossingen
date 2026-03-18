3d19ba13585b9e732affab73f0716dfdcbcef368
Stefaan Samyn <stefaan.samyn@hogent.be>
2026-03-18 11:45:25 +0100

    Update Simon

--- h05-oef04-simon-modeloplossing/src/main/java/gui/KiesSpelKleurenScherm.java
+++ h05-oef04-simon-modeloplossing/src/main/java/gui/KiesSpelKleurenScherm.java
@@ -1,8 +1,5 @@
 package gui;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import domein.DomeinController;
 import domein.Simon;
 import javafx.collections.FXCollections;
@@ -16,55 +13,56 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import util.Kleur;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class KiesSpelKleurenScherm extends VBox {
-	private final DomeinController dc;
-	private Kleur[] gekozenKleuren;
-	private Label lblFeedback;
+    private final DomeinController dc;
+    private final Kleur[] gekozenKleuren;
+    private Label lblFeedback;
 
-	public KiesSpelKleurenScherm(DomeinController dc) {
-		this.dc = dc;
-		gekozenKleuren = new Kleur[Simon.AANTAL_KLEUREN];
-		bouwScherm();
-	}
+    public KiesSpelKleurenScherm(DomeinController dc) {
+        this.dc = dc;
+        gekozenKleuren = new Kleur[Simon.AANTAL_KLEUREN];
+        bouwScherm();
+    }
 
-	private void bouwScherm() {
-		this.setPadding(new Insets(20));
-		this.setSpacing(10);
-		this.setAlignment(Pos.CENTER);
-		Label lblKiesKleuren = new Label(String.format("Kies de %d kleuren", Simon.AANTAL_KLEUREN));
-		lblKiesKleuren.setFont(Font.font("Montserrat", FontWeight.NORMAL, 20));
-		this.getChildren().add(lblKiesKleuren);
+    private void bouwScherm() {
+        setPadding(new Insets(20));
+        setSpacing(10);
+        setAlignment(Pos.CENTER);
+        Label lblKiesKleuren = new Label(String.format("Kies de %d kleuren", Simon.AANTAL_KLEUREN));
+        lblKiesKleuren.setFont(Font.font("Montserrat", FontWeight.NORMAL, 20));
+        getChildren().add(lblKiesKleuren);
 
-		List<String> kleurenStrings = new ArrayList<>();
-		for (Kleur k : Kleur.values()) {
-			kleurenStrings.add(k.toString());
-		}
-		for (int i = 0; i < Simon.AANTAL_KLEUREN; i++) {
-			final int kleurIndex = i;
-			ComboBox<String> cmbKleuren = new ComboBox<>(FXCollections.observableList(kleurenStrings));
-			cmbKleuren.setPromptText(String.format("Kies kleur %d", i + 1));
-			this.getChildren().add(cmbKleuren);
-			cmbKleuren
-					.setOnAction(evt -> registreerKleur(kleurIndex, cmbKleuren.getSelectionModel().getSelectedItem()));
+        List<Kleur> kleuren = Arrays.asList(Kleur.values());
+        for (int i = 0; i < Simon.AANTAL_KLEUREN; i++) {
+            final int kleurIndex = i;
+            ComboBox<Kleur> cmbKleuren = new ComboBox<>(FXCollections.observableList(kleuren));
+            cmbKleuren.setPromptText(String.format("Kies kleur %d", i + 1));
+            getChildren().add(cmbKleuren);
+            cmbKleuren
+                    .setOnAction(evt -> registreerKleur(kleurIndex, cmbKleuren.getSelectionModel().getSelectedItem()));
 
-		}
-		Button btnStartSpel = new Button("Start spel");
-		btnStartSpel.setOnAction(evt -> startSpel());
-		lblFeedback = new Label();
-		this.getChildren().addAll(btnStartSpel, lblFeedback);
-	}
+        }
+        Button btnStartSpel = new Button("Start spel");
+        btnStartSpel.setOnAction(evt -> startSpel());
+        lblFeedback = new Label();
+        this.getChildren().addAll(btnStartSpel, lblFeedback);
+    }
 
-	private void startSpel() {
-		try {
-			dc.startNieuwSpel(gekozenKleuren);
-			this.getScene().setRoot(new ToonNieuweKleurScherm(dc));
-		} catch (Exception e) {
-			lblFeedback.setText(e.getMessage());
-		}
-	}
+    private void startSpel() {
+        try {
+            dc.startNieuwSpel(gekozenKleuren);
+            getScene().setRoot(new ToonNieuweKleurScherm(dc));
+        } catch (Exception e) {
+            lblFeedback.setText(e.getMessage());
+        }
+    }
 
-	private void registreerKleur(int kleurIndex, String selectedItem) {
-		lblFeedback.setText("");
-		gekozenKleuren[kleurIndex] = Kleur.valueOf(selectedItem);
-	}
+    private void registreerKleur(int kleurIndex, Kleur geselecteerdeKleur) {
+        lblFeedback.setText("");
+        gekozenKleuren[kleurIndex] = geselecteerdeKleur;
+    }
+
 }
